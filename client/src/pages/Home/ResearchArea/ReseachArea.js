42f8ea15c6fe0dec202047afc44c5344db02bf43
gyanendra verma <gyanendra.verma@oodlestechnologies.com>
2019-08-27 11:42:34 +0530

    added animation in 'about the product' section and 'research area' secion

--- client/src/pages/Home/ResearchArea/ReseachArea.js
+++ client/src/pages/Home/ResearchArea/ReseachArea.js
@@ -12,7 +12,7 @@ function ResearchArea(props) {
             <InView>
               {({ inView, ref }) => (
                 <div ref={ref}>
-                  <div className={`${inView ? 'section-title' : ''}`}>
+                  <div className={`${inView ? 'section-title animated zoomIn' : ''}`}>
                     <h1 className="font-weight-bold">RESEARCH AREAS</h1>
                   </div>
                 </div>
@@ -20,41 +20,49 @@ function ResearchArea(props) {
             </InView>
           </div>
           <div className="row text-center center-card">
-            <div className="col-md-6 col-lg-6">
-              <a
-                className="card mx-4 research-card rounded-img"
-                href="/Military"
-              >
-                <img
-                  className="card-img-top"
-                  src="/img/ResearchAreaImg/Group-2491.svg"
-                  alt=""
-                />
-                <div className="card-body text-center">
-                  <h5>For Soldiers</h5>
-                  <p className="card-text px-4 mt-3 color">
-                    We use sensor-enabled, cloud-based platform for
-                    individualized brain modeling of Soldiers.
+            <InView>
+              {({ inView, ref }) => (
+                <div ref={ref} className={`col-md-6 col-lg-6 ${inView ? 'animated slideInLeft' : ''}`}>
+                  <a
+                    className="card mx-4 research-card rounded-img"
+                    href="/Military"
+                  >
+                    <img
+                      className="card-img-top"
+                      src="/img/ResearchAreaImg/Group-2491.svg"
+                      alt=""
+                    />
+                    <div className="card-body text-center">
+                      <h5>For Soldiers</h5>
+                      <p className="card-text px-4 mt-3 color">
+                        We use sensor-enabled, cloud-based platform for
+                        individualized brain modeling of Soldiers.
                   </p>
+                    </div>
+                  </a>
                 </div>
-              </a>
-            </div>
-            <div className="col-md-6 col-lg-6">
-              <a className="card mx-4 research-card rounded-img">
-                <img
-                  className="card-img-top"
-                  src="/img/ResearchAreaImg/Group-2492.svg"
-                  alt=""
-                />
-                <div className="card-body text-center">
-                  <h5>For Athletes</h5>
-                  <p className="card-text px-4 mt-3 color">
-                    We utilize data from the customized computer models to
-                    approximate an Athlete&apos;s brain’s response to injuries.
+              )}
+            </InView>
+            <InView>
+              {({ inView, ref }) => (
+                <div ref={ref} className={`col-md-6 col-lg-6 ${inView ? 'animated slideInRight' : ''}`}>
+                  <a className="card mx-4 research-card rounded-img">
+                    <img
+                      className="card-img-top"
+                      src="/img/ResearchAreaImg/Group-2492.svg"
+                      alt=""
+                    />
+                    <div className="card-body text-center">
+                      <h5>For Athletes</h5>
+                      <p className="card-text px-4 mt-3 color">
+                        We utilize data from the customized computer models to
+                        approximate an Athlete&apos;s brain’s response to injuries.
                   </p>
+                    </div>
+                  </a>
                 </div>
-              </a>
-            </div>
+              )}
+            </InView>
           </div>
         </div>
       </div>
