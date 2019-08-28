81400ca1126f7a95f1d34c9de30a0aae35ceac91
gyanendra verma <gyanendra.verma@oodlestechnologies.com>
2019-08-28 18:37:52 +0530

    made the component for dark theme chooser

---
+++ client/src/components/DarkMode.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+class DarkMode extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      visibility:{display:'none'}
+    };
+  }
+
+  toggleVisiblity = () => {
+    if (this.state.visibility.display === 'none')
+      this.setState({ visibility: { display: 'block' } })
+    else
+    this.setState({ visibility: { display: 'none' } })
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <div className="dark-mode-container text-center d-flex justify-content-center align-items-center">
+          <img onClick={this.toggleVisiblity} className="" src="/img/icon/darkModeSetting.svg" alt="" />
+        </div>
+        <div style={this.state.visibility} className="dark-mode-chooser text-center  justify-content-center align-items-center">
+          <div></div>
+          <div></div>
+
+        </div>
+      </React.Fragment>
+    );
+  }
+}
+
+export default DarkMode;
