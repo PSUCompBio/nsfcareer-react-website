938725d9061a8bf52ff919723dcfd0d3c0da1aec
gyanendra verma <gyanendra.verma@oodlestechnologies.com>
2019-08-28 16:52:46 +0530

    fix brain icon overlapping  of login and signup component

--- client/src/components/Authentication/LoginComponent.js
+++ client/src/components/Authentication/LoginComponent.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Link, Redirect, withRouter } from "react-router-dom";
-import Index from "../../index"
-import Footer from '../Footer'
+import {
+  BrowserRouter as Router,
+  Link,
+  Redirect,
+  withRouter
+} from 'react-router-dom';
+import Index from '../../index';
+import Footer from '../Footer';
 
 import { formDataToJson } from '../../utilities/utility';
 import { logIn, logInFirstTime } from '../../apis';
@@ -12,18 +17,16 @@ import '../../mixed_style.css';
 class Login extends React.Component {
   constructor(props) {
     super(props);
-    console.log("PROP SRECIVED",props);
+    console.log('PROP SRECIVED', props);
     this.state = {
-
       tempPasswordRequired: false,
       isLoginError: false,
-      loginErrorCode: "",
+      loginErrorCode: '',
       isLoading: false,
-      isSignInSuccessed : false
-    }
+      isSignInSuccessed: false
+    };
     this.handleSubmit = this.handleSubmit.bind(this);
   }
-
 
   isAdminType = (userType) => {
     if (userType === 'Admin') {
@@ -31,12 +34,12 @@ class Login extends React.Component {
     } else {
       return false;
     }
-  }
+  };
 
   handleSubmit(e) {
-      console.log("SIGNIN IN CLICKED");
+    console.log('SIGNIN IN CLICKED');
     e.preventDefault();
-    e.persist(); 
+    e.persist();
     //temporary setting authentication to change nav bar tabs
     this.props.isAuthenticated(true);
     console.log('LOGIN API CALLED');
@@ -44,156 +47,173 @@ class Login extends React.Component {
     this.setState({
       isLoginError: false,
       isLoading: true
-    })
+    });
     // converting formData to JSON
-    const formJsonData = formDataToJson(formData)
+    const formJsonData = formDataToJson(formData);
     if (this.state.tempPasswordRequired) {
       // call API of first Time Login with Temporary Password
-      logInFirstTime(formJsonData).then((response) => {
-        if (response.data.message === "success") {
+      logInFirstTime(formJsonData)
+        .then((response) => {
+          if (response.data.message === 'success') {
             this.setState({
               isLoading: false,
-              isSignInSuccessed : true
-            })
-          // login user
-          // ReactDOM.render(
-          //
-          //   <Router>
-          //
-          //     <Index isAuthenticated={true} isAdmin={this.isAdminType(response.data.user_type)} />
-          //   </Router>
-          //   ,
-          //   document.getElementById('root')
-          // );
-        }
-        else {
-          // show error
-          this.setState({
-            loginError: response.data.error,
-            isLoginError: true,
-            isLoading: false
-          })
-
-        }
-      }).catch((err) => {
-        console.log(err);
-
-      })
-    }
-    else {
-      logIn(formJsonData).then((response) => {
-        // this.refs.signInForm.reset();
-        console.log("Login ", response);
-        if (response.data.message === "success") {
-          if (response.data.status === "FORCE_CHANGE_PASSWORD") {
-            this.setState({
-              tempPasswordRequired: true,
-              isLoading: false
-            })
-          }
-          else {
-            // login redirect code here
-            this.setState({
-              isLoading: false,
-              isSignInSuccessed : true
-            })
-
+              isSignInSuccessed: true
+            });
+            // login user
             // ReactDOM.render(
+            //
             //   <Router>
+            //
             //     <Index isAuthenticated={true} isAdmin={this.isAdminType(response.data.user_type)} />
             //   </Router>
             //   ,
             //   document.getElementById('root')
             // );
+          } else {
+            // show error
+            this.setState({
+              loginError: response.data.error,
+              isLoginError: true,
+              isLoading: false
+            });
           }
-        }
-        else {
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    } else {
+      logIn(formJsonData)
+        .then((response) => {
+          // this.refs.signInForm.reset();
+          console.log('Login ', response);
+          if (response.data.message === 'success') {
+            if (response.data.status === 'FORCE_CHANGE_PASSWORD') {
+              this.setState({
+                tempPasswordRequired: true,
+                isLoading: false
+              });
+            } else {
+              // login redirect code here
+              this.setState({
+                isLoading: false,
+                isSignInSuccessed: true
+              });
 
-          this.setState({
-            isLoginError: true,
-            isLoading: false,
-            loginError: response.data.error
-          })
+              // ReactDOM.render(
+              //   <Router>
+              //     <Index isAuthenticated={true} isAdmin={this.isAdminType(response.data.user_type)} />
+              //   </Router>
+              //   ,
+              //   document.getElementById('root')
+              // );
+            }
+          } else {
+            this.setState({
+              isLoginError: true,
+              isLoading: false,
+              loginError: response.data.error
+            });
+          }
 
-        }
-
-        // Now update the state with data that we added
-
-
-      }).catch((err) => {
-        e.target.reset();
-        // catch error
-        console.log("error : ", err);
-
-      })
+          // Now update the state with data that we added
+        })
+        .catch((err) => {
+          e.target.reset();
+          // catch error
+          console.log('error : ', err);
+        });
     }
   }
 
   render() {
     return (
-
       <div className="container-fluid pl-0 pr-0 overflow-hidden">
-      {this.state.isSignInSuccessed?
-          <Redirect to="/Profile"/>
-          :null
-      }
+        {this.state.isSignInSuccessed ? <Redirect to="/Profile" /> : null}
         <div className="row login">
-          <div className="col-md-6 col-lg-6 offset-md-3 mt-10">
+          <div className="col-md-5 ml-md-auto mr-md-auto mt-10">
             <div className="card card-border">
               <div className="card-body">
-                <div className="text-center brain-icon-container">
-                  <div className="text-center brain-icon">
-                    <img src="img/icon/brain.png" alt="" />
-                  </div>
-                </div>
-                 <form onSubmit={this.handleSubmit} ref="signInForm">
-                <div className="input-group mb-5">
-                  <div className="input-group-prepend">
-                    <span className="input-group-text" id="basic-addon1">
-                      <img src="img/icon/user.svg" alt="" />
-                    </span>
-                  </div>
-                  <input type="text" className="form-control" name="user_name" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1" />
-                </div>
-                <div className="input-group mb-5">
-                  <div className="input-group-prepend">
-                    <span className="input-group-text" id="basic-addon1">
-                      <img src="img/icon/lock.svg" alt="" />
-                    </span>
-                  </div>
-                  <input type="password" className="form-control" name="password" placeholder="Password" aria-label="Password" aria-describedby="basic-addon1" />
+                <div className="text-center brain-icon">
+                  <img src="img/icon/brain.png" alt="" />
                 </div>
 
-                {
-                      this.state.tempPasswordRequired ?
-                        <div className="input-group mb-1">
-                          <div className="input-group-prepend">
-                            <span className="input-group-text" id="basic-addon1"><img src="img/icon/lock.svg" alt="" /></span>
-                          </div>
-                          <input type="password" className="form-control" name="new_password" placeholder="New Password (Min. 8 digit password)" aria-label="Password" aria-describedby="basic-addon1" />
-                        </div>
-                        : null
-                }
+                <form onSubmit={this.handleSubmit} ref="signInForm">
+                  <div className="input-group mb-5">
+                    <div className="input-group-prepend">
+                      <span className="input-group-text" id="basic-addon1">
+                        <img src="img/icon/user.svg" alt="" />
+                      </span>
+                    </div>
+                    <input
+                      type="text"
+                      className="form-control"
+                      name="user_name"
+                      placeholder="Username"
+                      aria-label="Username"
+                      aria-describedby="basic-addon1"
+                    />
+                  </div>
+                  <div className="input-group mb-1">
+                    <div className="input-group-prepend">
+                      <span className="input-group-text" id="basic-addon1">
+                        <img src="img/icon/lock.svg" alt="" />
+                      </span>
+                    </div>
+                    <input
+                      type="password"
+                      className="form-control"
+                      name="password"
+                      placeholder="Password"
+                      aria-label="Password"
+                      aria-describedby="basic-addon1"
+                    />
+                  </div>
 
-                <div>
-                  <Link to="/Forgot-Password" className="float-right forgot-pswd">Forgot password?</Link>
-                </div>
-                <button type="submit" className="btn btn-primary log-in-btn btn-block mt-5">LOG IN</button>
+                  {this.state.tempPasswordRequired ? (
+                    <div className="input-group mb-1">
+                      <div className="input-group-prepend">
+                        <span className="input-group-text" id="basic-addon1">
+                          <img src="img/icon/lock.svg" alt="" />
+                        </span>
+                      </div>
+                      <input
+                        type="password"
+                        className="form-control"
+                        name="new_password"
+                        placeholder="New Password (Min. 8 digit password)"
+                        aria-label="Password"
+                        aria-describedby="basic-addon1"
+                      />
+                    </div>
+                  ) : null}
+
+                  <div>
+                    <Link
+                      to="/Forgot-Password"
+                      className="float-right forgot-pswd"
+                    >
+                      Forgot password?
+                    </Link>
+                  </div>
+                  <button
+                    type="submit"
+                    className="btn btn-primary log-in-btn btn-block mt-5"
+                  >
+                    LOG IN
+                  </button>
                 </form>
-                {
-                    this.state.isLoading ?
-                    <div className="d-flex justify-content-center center-spinner">
-                         <div className="spinner-border text-primary" role="status" >
-        <span className="sr-only">Loading...</span>
-      </div>
-             </div>:null
-                  }
-                  {this.state.isLoginError ?
-                      <div class="alert alert-info api-response-alert" role="alert">
+                {this.state.isLoading ? (
+                  <div className="d-flex justify-content-center center-spinner">
+                    <div className="spinner-border text-primary" role="status">
+                      <span className="sr-only">Loading...</span>
+                    </div>
+                  </div>
+                ) : null}
+                {this.state.isLoginError ? (
+                  <div class="alert alert-info api-response-alert" role="alert">
                     <strong>Failed! </strong> {this.state.loginError}.
-</div>
-                    : null
-                    }
+                  </div>
+                ) : null}
                 <div className="text-center">
                   <p className="mt-4 sign-up-link">
                     Don't have an account?{' '}
