948ce1eb9b677dac0dd14300680f177f748d525f
gyanendra verma <gyanendra.verma@oodlestechnologies.com>
2019-09-04 15:39:33 +0530

    Implemented logic to manipulate values of single component

--- client/src/components/PenstateUniversity.js
+++ client/src/components/PenstateUniversity.js
@@ -5,14 +5,20 @@ class PenstateUniversity extends React.Component {
   constructor() {
     super();
     this.state = {
-      impactCount: 2,
-      avgLoad: 0.02,
-      alerts: 0
+      circleValues: []
     };
   }
 
-  impactLoadAlertsValue = () => {
-    
+  impactLoadAlertsValue = ()=>{
+    if (this.props.location.pathname === '/TeamRoster')
+      this.setState({circleValues:[2,6,8]})
+    else if (this.props.location.pathname === '/CommanderTeamView')
+      this.setState({ circleValues: [2, 0.02, 0] })
+      else if (this.props.location.pathname === '/CommanderDataTable')
+      this.setState({circleValues:[2,0.02,0]})
+  }
+  componentWillMount() {
+    this.impactLoadAlertsValue();
   }
 
   render() {
@@ -43,19 +49,19 @@ class PenstateUniversity extends React.Component {
         <div className="col-md-5 d-flex mt-3 justify-content-center align-items-center">
           <div className="counter-container ml-md-auto mr-md-auto text-center">
             <div className="team-view-counter mb-2 ">
-              <p>{this.state.impactCount}</p>
+              <p>{this.state.circleValues[0]}</p>
             </div>
             <p>Impacts</p>
           </div>
           <div className="counter-container ml-md-auto mr-md-auto text-center">
             <div className="team-view-counter mb-2 ">
-              <p> {this.state.avgLoad} </p>
+              <p> {this.state.circleValues[1]} </p>
             </div>
             <p>Avg Load</p>
           </div>
           <div className="counter-container ml-md-auto mr-md-auto text-center">
             <div className="team-view-counter mb-2 ">
-              <p> {this.state.alerts} </p>
+              <p> {this.state.circleValues[2]} </p>
             </div>
             <p>Alerts</p>
           </div>
