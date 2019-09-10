98cd7b8317b8d9b9f59ebcef6258bfeb624d034a
gyanendra verma <gyanendra.verma@oodlestechnologies.com>
2019-09-10 11:10:46 +0530

    added larger text at the top

--- client/src/components/Authentication/LoginComponent.js
+++ client/src/components/Authentication/LoginComponent.js
@@ -110,6 +110,16 @@ class Login extends React.Component {
         <div className="container pl-0 pr-0 overflow-hidden">
           {this.state.isSignInSuccessed ? <Redirect to="/dashboard" /> : null}
           <div className="row login">
+            <div className="col-md-12 mt-5 mb-5">
+              <div className="card text-center ">
+                <p className="top-heading__login">
+                  The Dashboard gives users and administers a cumulative
+                  overview, as well as an in-depth analysis on each head
+                  acceleration event.
+                </p>
+              </div>
+            </div>
+
             <div className="col-md-6 mb-5 p-3">
               <div id="dashboard-view" className="text-right dashboard-mock">
                 <img
