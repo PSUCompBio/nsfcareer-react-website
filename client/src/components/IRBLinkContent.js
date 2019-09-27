fa5ba663becb603a3a0722d2f2669da7f2aeb38e
gyanendra verma <gyanendra.verma@oodlestechnologies.com>
2019-09-27 10:40:30 +0530

    writing content for the IRB link.

--- client/src/components/IRBLinkContent.js
+++ client/src/components/IRBLinkContent.js
@@ -883,6 +883,140 @@ class IRBLinkContent extends React.Component {
                     research context, the IRB will generally waive the
                     requirement to obtain written documentation of consent.
                   </p>
+                  <Collapsible trigger="For the research portion of the work">
+                    <p>Only a written consent will be obtained. </p>
+                  </Collapsible>
+                  <Collapsible trigger="In the youth and community educational and outreach of the work">
+                    <p>Only a written consent will be obtained. </p>
+                  </Collapsible>
+                  <Collapsible trigger="In the university engineering educational portion of the work">
+                    <p>Only a written consent will be obtained. </p>
+                  </Collapsible>
+                </Collapsible>
+              </Collapsible>
+              <Collapsible trigger="5.3 Consent – Other Considerations ">
+                <Collapsible trigger="5.3.1 Non-English Speaking Subjects">
+                  <p>
+                    Indicate what language(s) other than English are understood
+                    by prospective subjects or representatives. If subjects who
+                    do not speak English will be enrolled, describe the process
+                    to ensure that the oral and written information provided to
+                    those subjects will be in that language. Indicate the
+                    language that will be used by those obtaining consent.
+                    Indicate whether the consent process will be documented in
+                    writing with the long form of the consent documentation or
+                    with the short form of the consent documentation. Review the
+                    “SOP: Written Documentation of Consent (HRP-091)” and the
+                    “Investigator Manual (HRP-103)” to ensure that you have
+                    provided sufficient information. HRP-091 and HRP-103 can be
+                    accessed by clicking the Library link in CATS IRB
+                    (http://irb.psu.edu). (Not Applicable). Based on the
+                    inclusion criteria, all the subjects should speak English.
+                  </p>
+                </Collapsible>
+                <Collapsible trigger="5.3.2 Cognitively Impaired Adults">
+                  <p>
+                    Refer to “CHECKLIST: Cognitively Impaired Adults (HRP-417)”
+                    for information about research involving cognitively
+                    impaired adults as subjects. HRP-417 can be accessed by
+                    clicking the Library link in CATS IRB (http://irb.psu.edu).
+                  </p>
+                  <Collapsible trigger="5.3.2.1 Capability of Providing Consent">
+                    <p>
+                      Describe the process to determine whether an individual is
+                      capable of consent.
+                    </p>
+                  </Collapsible>
+                  <Collapsible trigger="5.3.2.2 Adults Unable To Consent">
+                    <p>
+                      Describe whether and how informed consent will be obtained
+                      from the legally authorized representative. Describe who
+                      will be allowed to provide informed consent. Describe the
+                      process used to determine these individual’s authority to
+                      consent to research. For research conducted in the state,
+                      review “SOP: Legally Authorized Representatives, Children
+                      and Guardians (HRP-013)” to be aware of which individuals
+                      in the state meet the definition of “legally authorized
+                      representative”. HRP-013 can be accessed by clicking the
+                      Library link in CATS IRB (http://irb.psu.edu). For
+                      research conducted outside of the state, provide
+                      information that describes which individuals are
+                      authorized under applicable law to consent on behalf of a
+                      prospective subject to their participation in the
+                      procedure(s) involved in this research. One method of
+                      obtaining this information is to have a legal counsel or
+                      authority review your protocol along with the definition
+                      of “children” in “SOP: Legally Authorized Representatives,
+                      Children, and Guardians (HRP-013).” HRP-013 can be
+                      accessed by clicking the Library link in CATS IRB
+                      (http://irb.psu.edu). Not applicable.
+                    </p>
+                  </Collapsible>
+                  <Collapsible trigger="5.3.2.3 Assent of Adults Unable to Consent">
+                    <p>
+                      Describe the process for assent of the subjects. Indicate
+                      whether assent will be required of all, some or none of
+                      the subjects. If some, indicate which subjects will be
+                      required to assent and which will not. If assent will not
+                      be obtained from some or all subjects, provide an
+                      explanation of why not. Describe whether assent of the
+                      subjects will be documented and the process to document
+                      assent. The IRB allows the person obtaining assent to
+                      document assent on the consent document and does not
+                      routinely require assent documents and does not routinely
+                      require subjects to sign assent documents. Not applicable.
+                    </p>
+                  </Collapsible>
+                </Collapsible>
+                <Collapsible trigger="5.3.3 Subjects who are not yet adults (infants, children, teenagers) ">
+                  <Collapsible trigger="5.3.3.1 Parental Permission">
+                    <p>
+                      Describe whether and how parental permission will be
+                      obtained. If permission will be obtained from individuals
+                      other than parents, describe who will be allowed to
+                      provide permission. Describe the process used to determine
+                      these individual’s authority to consent to each child’s
+                      general medical care. For research conducted in the state,
+                      review “SOP: Legally Authorized Representatives, Children
+                      and Guardians (HRP-013)” to be aware of which individuals
+                      in the state meet the definition of “children”. HRP-013
+                      can be accessed by clicking the Library link in CATS IRB
+                      (http://irb.psu.edu). For research conducted outside of
+                      the state, provide information that describes which
+                      persons have not attained the legal age for consent to
+                      treatments or procedures involved in the research, under
+                      the applicable law of the jurisdiction in which research
+                      will be conducted. One method of obtaining this
+                      information is to have a legal counsel or authority review
+                      your protocol along with the definition of “children” in
+                      “SOP: Legally Authorized Representatives, Children, and
+                      Guardians (HRP-013).” HRP-013 can be accessed by clicking
+                      the Library link in CATS IRB (http://irb.psu.edu). The
+                      consent process will take place at the locations of the
+                      participating research teams (e.g. State College High
+                      School) and on the campus of Penn State University for PSU
+                      collegiate athletes and on the fields of state college
+                      youth football league.
+                    </p>
+                    <Collapsible trigger="For the research portion of the work">
+                      <p>Only a written consent will be obtained. </p>
+                    </Collapsible>
+                    <Collapsible trigger="In the youth and community educational and outreach of the work">
+                      <p>Only a written consent will be obtained. </p>
+                    </Collapsible>
+                    <Collapsible trigger="In the university engineering educational portion of the work">
+                      <p>Not applicable. </p>
+                    </Collapsible>
+                  </Collapsible>
+                  <Collapsible trigger="5.3.3.2 Assent of subjects who are not yet adults">
+                    <p>
+                      Indicate whether assent will be obtained from all, some,
+                      or none of the children. If assent will be obtained from
+                      some children, indicate which children will be required to
+                      assent. When assent of children is obtained describe
+                      whether and how it will be documented.
+                    </p>
+                  </Collapsible>
                 </Collapsible>
               </Collapsible>
             </Collapsible>
