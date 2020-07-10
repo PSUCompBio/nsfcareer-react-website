4a6f574289eb021bf8771886f1e710adeff1b598
Raman Kumar <ramana.kumar@brihaspatitech.com>
2020-07-10 20:45:44 +0530

    Created plot option for Gs or m/s^2

--- client/src/components/DashboardEventsChart/HeadAccelerationAllEvents.js
+++ client/src/components/DashboardEventsChart/HeadAccelerationAllEvents.js
@@ -1,15 +1,11 @@
 import React from 'react';
-import { Line, defaults  } from 'react-chartjs-2';
-
-// defaults.global.maintainAspectRatio = false
+import { Line } from 'react-chartjs-2';
 
 const options = {
     responsive: true,
-    maintainAspectRatio: false,
-    fill: false,
+    maintainAspectRatio: true,
     legend: {
         display: true,
-        fontSize: 3
     },
     plugins: {
         datalabels: {
@@ -21,7 +17,7 @@ const options = {
         yAxes: [{
             scaleLabel: {
                 display: true,
-                labelString: 'Linear Acceleration'
+                labelString: 'Linear Acceleration (Gs)'
             },
             id: 'A',
             position: 'left',
@@ -31,7 +27,7 @@ const options = {
         }, {
             scaleLabel: {
                 display: true,
-                labelString: 'Angular Acceleration'
+                labelString: 'Angular Acceleration (rad/s2)'
             },
             id: 'B',
             position: 'right',
@@ -50,42 +46,42 @@ const options = {
 };
 
 
-const optionsMaxStrain = {
-    responsive: true,
-    maintainAspectRatio : false,
-    plugins: {
-        datalabels: {
-            // hide datalabels for all datasets
-            display: false
-        }
-    },
-    scales: {
-        yAxes: [{
-            scaleLabel: {
-                display: true,
-                labelString: 'Max Strain'
-            },
-            ticks: {
-                min: 0
-            }
-        }],
-        xAxes: [{
+// const optionsMaxStrain = {
+//     responsive: true,
+//     maintainAspectRatio: true,
+//     plugins: {
+//         datalabels: {
+//             // hide datalabels for all datasets
+//             display: false
+//         }
+//     },
+//     scales: {
+//         yAxes: [{
+//             scaleLabel: {
+//                 display: true,
+//                 labelString: 'Max Strain'
+//             },
+//             ticks: {
+//                 min: 0
+//             }
+//         }],
+//         xAxes: [{
 
-            scaleLabel: {
-                display: true,
-                labelString: 'Time (ms)'
-            }
-        }]
-    }
-};
+//             scaleLabel: {
+//                 display: true,
+//                 labelString: 'Time (ms)'
+//             }
+//         }]
+//     }
+// };
 
 class HeadAccelerationAllEvents extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
+            linear_unit: 'gs',
             data: {
                 labels: this.props.data.time,
-                fill: false, 
                 datasets: [{
                     lineTension: 0.1,
                     label: "X Linear Acceleration",
@@ -93,7 +89,7 @@ class HeadAccelerationAllEvents extends React.Component {
                     borderColor: '#1987DD',
                     yAxisID: 'A',
                     fill: false,
-                    data: this.props.data.linear_acceleration['xv'] ? this.props.data.linear_acceleration['xv'] : [],
+                    data: this.props.data.linear_acceleration['xv-g'] ? this.props.data.linear_acceleration['xv-g'] : [],
                 }, {
                     lineTension: 0.1,
                     label: "Y Linear Acceleration",
@@ -101,7 +97,7 @@ class HeadAccelerationAllEvents extends React.Component {
                     borderColor: '#ff0000',
                     yAxisID: 'A',
                     fill: false,
-                    data: this.props.data.linear_acceleration['yv'] ? this.props.data.linear_acceleration['yv'] : [],
+                    data: this.props.data.linear_acceleration['yv-g'] ? this.props.data.linear_acceleration['yv-g'] : [],
                 }, {
                     lineTension: 0.1,
                     label: "Z Linear Acceleration",
@@ -109,7 +105,7 @@ class HeadAccelerationAllEvents extends React.Component {
                     borderColor: '#00c04a',
                     yAxisID: 'A',
                     fill: false,
-                    data: this.props.data.linear_acceleration['zv'] ? this.props.data.linear_acceleration['zv'] : [],
+                    data: this.props.data.linear_acceleration['zv-g'] ? this.props.data.linear_acceleration['zv-g'] : [],
                 }, {
                     lineTension: 0.1,
                     label: "X Angular Acceleration",
@@ -137,39 +133,62 @@ class HeadAccelerationAllEvents extends React.Component {
                 }]
 
             },
-            dataMaxStrain: {
-                labels: this.props.data.time,
-                datasets: [{
-                    lineTension: 0.1,
-                    label: "Max. Strain Vs Time",
-                    backgroundColor: '#7CB5EC',
-                    borderColor: '#1987DD',
-                    data: this.props.data.max_linear_acceleration,
-                }]
+            // dataMaxStrain: {
+            //     labels: this.props.data.time,
+            //     datasets: [{
+            //         lineTension: 0.1,
+            //         label: "Max. Strain Vs Time",
+            //         backgroundColor: '#7CB5EC',
+            //         borderColor: '#1987DD',
+            //         data: this.props.data.max_linear_acceleration,
+            //     }]
 
-            },
+            // },
             is_selfie_simulation_file_uploaded: props.is_selfie_simulation_file_uploaded,
             imageUrl: props.imageUrl
         };
+        this.handleChange = this.handleChange.bind(this);
+    }
+ 
+    handleChange = (event) => {
+        let temp_data = this.state.data;
+        
+        if (event.target.value === 'ms') {
+            options.scales.yAxes[0].scaleLabel.labelString = 'Linear Acceleration (m/s2)';
+            temp_data.datasets[0].data = this.props.data.linear_acceleration['xv'] ? this.props.data.linear_acceleration['xv'] : [];
+            temp_data.datasets[1].data = this.props.data.linear_acceleration['yv'] ? this.props.data.linear_acceleration['yv'] : [];
+            temp_data.datasets[2].data = this.props.data.linear_acceleration['zv'] ? this.props.data.linear_acceleration['zv'] : [];          
+        } else {
+            options.scales.yAxes[0].scaleLabel.labelString = 'Linear Acceleration (Gs)';
+            temp_data.datasets[0].data = this.props.data.linear_acceleration['xv-g'] ? this.props.data.linear_acceleration['xv-g'] : [];
+            temp_data.datasets[1].data = this.props.data.linear_acceleration['yv-g'] ? this.props.data.linear_acceleration['yv-g'] : [];
+            temp_data.datasets[2].data = this.props.data.linear_acceleration['zv-g'] ? this.props.data.linear_acceleration['zv-g'] : [];
+        }
+        this.setState({
+            linear_unit: event.target.value,
+            data: temp_data
+        });
     }
 
     render() {
+       
         return (
             <div className="position-relative animated fadeInRight  bg-white acc-evnt">
                 {/*<div  data-descr={`${new Date(Number(this.props.data.timestamp)).toDateString()} ${new Date(Number(this.props.data.timestamp)).toLocaleTimeString()}`} className="position-relative head-acc-evnt-chart pl-2 pr-2">
       <div  data-descr={`${new Date(Number(this.props.data.timestamp)).toDateString()} ${this.props.data.record_time}`} className="position-relative head-acc-evnt-chart pl-2 pr-2">*/}
                 <div data-descr={`${this.props.data.sensor_data['impact-date'] ? this.props.data.sensor_data['impact-date'] : '2020-01-01'} ${this.props.data.sensor_data['impact-time'] ? this.props.data.sensor_data['impact-time'] : ''}`} className="position-relative head-acc-evnt-chart pl-2 pr-2">
                     <div className="brain-card-pt-2-5 row pl-4 pr-4 pb-4 dark-bg text-center ">
-
-
-
-
-                        <div className="Individual-Head-Acceleration-player-dash-chart">
-
-                            <Line data={this.state.data} options={options} />
-
+                        <div className="col-md-8 ">
+                            <Line data={this.state.data} options={options} redraw={true}/>
+                            <div>
+                                <label>Linear Acceleration </label>
+                                <select id="linear_unit" onChange={this.handleChange} value={this.state.linear_unit} style={{marginLeft: '10px'}}>
+                                    <option value="gs">G(s)</option>
+                                    <option value="ms">m/s2</option>
+                                </select>
+                            </div>
                         </div>
-                        <div className="Individual-Head-Acceleration-player-dash-image ">
+                        <div className="col-md-4 d-flex ">
 
                             <div className="col-md-12">
                                 {/*{(this.state.is_selfie_simulation_file_uploaded)?<div><img className={`img fluid ${'svg'}`} src="/img/icon/brainEvnt.svg" alt="" /><img width="60%"  height="60%" className={`img-fluid ${'svg'}`} src={this.state.imageUrl} alt="" /> </div> : <img className={`img fluid ${'svg'}`} width="60%"  height="60%" src="/img/icon/brainEvnt.svg" alt="" />}*/}
