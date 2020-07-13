d888fad5a1cea23ca12389519ea60e6e53da99e6
Raman Kumar <ramana.kumar@brihaspatitech.com>
2020-07-13 13:42:25 +0530

    Added common section for linear unit

--- client/src/components/DashboardEventsChart/HeadAccelerationAllEvents.js
+++ client/src/components/DashboardEventsChart/HeadAccelerationAllEvents.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Line} from 'react-chartjs-2';
+import { Line } from 'react-chartjs-2';
 
 const options = {
     responsive: true,
@@ -46,44 +46,13 @@ const options = {
     }
 };
 
-
-const optionsMaxStrain = {
-    responsive: true,
-    maintainAspectRatio : false,
-    plugins: {
-        datalabels: {
-            // hide datalabels for all datasets
-            display: false
-        }
-    },
-    scales: {
-        yAxes: [{
-            scaleLabel: {
-                display: true,
-                labelString: 'Max Strain'
-            },
-            ticks: {
-                min: 0
-            }
-        }],
-        xAxes: [{
-
-            scaleLabel: {
-                display: true,
-                labelString: 'Time (ms)'
-            }
-        }]
-    }
-};
-
 class HeadAccelerationAllEvents extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            linear_unit: 'gs',
             data: {
                 labels: this.props.data.time,
-                fill: false, 
+                fill: false,
                 datasets: [{
                     lineTension: 0.1,
                     label: "X Linear Acceleration",
@@ -151,81 +120,39 @@ class HeadAccelerationAllEvents extends React.Component {
         };
     }
 
-    handleChange = (event) => {
-        let temp_data = this.state.data;
-        
-        if (event.target.value === 'ms') {
+    static getDerivedStateFromProps (props, state) {
+        let temp_data = state.data;
+        console.log(props.linearUnit)
+        if (props.linearUnit === 'ms') {
             options.scales.yAxes[0].scaleLabel.labelString = 'Linear Acceleration (m/s2)';
-            temp_data.datasets[0].data = this.props.data.linear_acceleration['xv'] ? this.props.data.linear_acceleration['xv'] : [];
-            temp_data.datasets[1].data = this.props.data.linear_acceleration['yv'] ? this.props.data.linear_acceleration['yv'] : [];
-            temp_data.datasets[2].data = this.props.data.linear_acceleration['zv'] ? this.props.data.linear_acceleration['zv'] : [];          
+            temp_data.datasets[0].data = props.data.linear_acceleration['xv'] ? props.data.linear_acceleration['xv'] : [];
+            temp_data.datasets[1].data = props.data.linear_acceleration['yv'] ? props.data.linear_acceleration['yv'] : [];
+            temp_data.datasets[2].data = props.data.linear_acceleration['zv'] ? props.data.linear_acceleration['zv'] : [];
         } else {
             options.scales.yAxes[0].scaleLabel.labelString = 'Linear Acceleration (Gs)';
-            temp_data.datasets[0].data = this.props.data.linear_acceleration['xv-g'] ? this.props.data.linear_acceleration['xv-g'] : [];
-            temp_data.datasets[1].data = this.props.data.linear_acceleration['yv-g'] ? this.props.data.linear_acceleration['yv-g'] : [];
-            temp_data.datasets[2].data = this.props.data.linear_acceleration['zv-g'] ? this.props.data.linear_acceleration['zv-g'] : [];
+            temp_data.datasets[0].data = props.data.linear_acceleration['xv-g'] ? props.data.linear_acceleration['xv-g'] : [];
+            temp_data.datasets[1].data = props.data.linear_acceleration['yv-g'] ? props.data.linear_acceleration['yv-g'] : [];
+            temp_data.datasets[2].data = props.data.linear_acceleration['zv-g'] ? props.data.linear_acceleration['zv-g'] : [];
         }
-        this.setState({
-            linear_unit: event.target.value,
+        return {
             data: temp_data
-        });
+        };
     }
 
     render() {
         return (
             <div className="position-relative animated fadeInRight  bg-white acc-evnt">
-                {/*<div  data-descr={`${new Date(Number(this.props.data.timestamp)).toDateString()} ${new Date(Number(this.props.data.timestamp)).toLocaleTimeString()}`} className="position-relative head-acc-evnt-chart pl-2 pr-2">
-      <div  data-descr={`${new Date(Number(this.props.data.timestamp)).toDateString()} ${this.props.data.record_time}`} className="position-relative head-acc-evnt-chart pl-2 pr-2">*/}
                 <div data-descr={`${this.props.data.sensor_data['impact-date'] ? this.props.data.sensor_data['impact-date'] : '2020-01-01'} ${this.props.data.sensor_data['impact-time'] ? this.props.data.sensor_data['impact-time'] : ''}`} className="position-relative head-acc-evnt-chart pl-2 pr-2">
                     <div className="brain-card-pt-2-5 row pl-4 pr-4 pb-4 dark-bg text-center ">
-
-
-
-
                         <div className="Individual-Head-Acceleration-player-dash-chart">
-
-                            <Line data={this.state.data} options={options} />
-
+                            <Line data={this.state.data} options={options} redraw={true} />
                         </div>
-
-                       
                         <div className="Individual-Head-Acceleration-player-dash-image ">
-
                             <div className="col-md-12">
-                                {/*{(this.state.is_selfie_simulation_file_uploaded)?<div><img className={`img fluid ${'svg'}`} src="/img/icon/brainEvnt.svg" alt="" /><img width="60%"  height="60%" className={`img-fluid ${'svg'}`} src={this.state.imageUrl} alt="" /> </div> : <img className={`img fluid ${'svg'}`} width="60%"  height="60%" src="/img/icon/brainEvnt.svg" alt="" />}*/}
                                 <div><img className={`img-fluid ${'svg'}`} width="100%" height="60%" src={this.props.data.simulation_image ? 'data:image/png;base64,' + this.props.data.simulation_image : '/img/icon/brainEvnt.svg'} alt="" />
                                 </div>
                             </div>
-                            {/*
-          <div className="col-md-4">
-    <div class="widget-chart-content">
-                <div class="widget-subheading">Position</div>
-                    <div class="widget-numbers">
-                        <span>DE/RT</span>
-                    </div>
-                </div>
-                <div class="widget-chart-content">
-                      <div class="widget-subheading">Impact Location</div>
-                          <div class="widget-numbers">
-                              <span>DE/RT</span>
-                          </div>
-                      </div>
-          </div>
-          <br/>
-
-*/}
-
                         </div>
-                        {/* <div className="col-md-4">
-                  <Line data={this.state.dataMaxStrain} options={optionsMaxStrain}/>
-          </div>*/}
-           <div className="col-md-12">
-                                <label>Linear Acceleration </label>
-                                <select id="linear_unit" onChange={this.handleChange} value={this.state.linear_unit} style={{marginLeft: '10px'}}>
-                                    <option value="gs">G(s)</option>
-                                    <option value="ms">m/s2</option>
-                                </select>
-                            </div>
                     </div>
                 </div>
             </div>
