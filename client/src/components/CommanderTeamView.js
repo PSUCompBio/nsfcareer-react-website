71a9b9eb4242d15121c861dd9326dfd4fb37dd99
Raman Kumar <ramana.kumar@brihaspatitech.com>
2020-08-06 11:17:08 +0530

    Displayed sorted table according to latest simulation

--- client/src/components/CommanderTeamView.js
+++ client/src/components/CommanderTeamView.js
@@ -333,7 +333,7 @@ class CommanderTeamView extends React.Component {
         const second = plus0(d.getSeconds())
         const rest = timestamp.toString().slice(-5)
       
-        return `${month}-${date}-${year} ${hour}:${minute}:${second}:${rest}`
+        return `${month}:${date}:${year} ${hour}:${minute}:${second}:${rest}`
       }
 
     militaryVersionOrNormal = () => {
